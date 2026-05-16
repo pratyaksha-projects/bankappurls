dd91a849d785b82154db892501a44d9dbb11555f
pratyaksha-projects <pratyakshasaini11@gmail.com>
2026-05-16 17:58:17 +0530

    Update urls.py

--- Bankappurls/core/urls.py
+++ Bankappurls/core/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('deposit/', views.deposit, name='deposit'),
-    path('withdraw/', views.withdraw, name='withdraw'),  # ✅ now matches views.py
+    path('withdraw/', views.withdraw, name='withdraw'), 
     path('transactions/', views.transaction_history, name='transactions'),
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),  # if you added profile
+    path('profile/', views.profile, name='profile'),
 ]
